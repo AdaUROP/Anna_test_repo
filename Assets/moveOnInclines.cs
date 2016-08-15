8c89139fa6b12d59db4751ad369a4121b9970c0f
Anna Sikkink <asikkink@hawaii.edu>
2016-08-14 21:49:10 -1000

    Added incline test project

---
+++ Assets/moveOnInclines.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveOnInclines : MonoBehaviour {
+
+    Vector3 initDistanceFromGround;
+
+	// Use this for initialization
+	void Start () {
+        //find the difference in distance from the camera to the ground
+	    initDistanceFromGround = gameObject.transform.position.y - GameObject.F
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //set the distance of the camera from the ground to match our initial value
+	
+	}
+}
