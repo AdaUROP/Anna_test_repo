ff9b9327bd3dc7890ab98f4387f74d44a9066a83
Anna Sikkink <asikkink@hawaii.edu>
2016-08-24 10:31:39 -1000

    blendshapes class and driver

---
+++ Assets/changeExpression.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeExpression : MonoBehaviour {
+
+    public float blendSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void blendShape(GameObject blend, float goal){
+
+        try {
+            SkinnedMeshRenderer smr = blend.GetComponent<SkinnedMeshRenderer>();
+            StartCoroutine(blendToGoal(smr, goal));
+        }
+        catch
+        {
+            Debug.Log("Object does not have a blendshape SkinnedMeshRenderer Component.");
+        }
+		
+	}
+	
+	//write a basic function to call the coroutines given an object to blend
+	
+	//coroutine should check goal against max and min, and see if the change is pos or neg (curr vs goal)
+	//then update the blend weight by the speed in a while loop until we get to the goal
+    IEnumerator blendToGoal(SkinnedMeshRenderer smr, float goal)
+    {
+        float currWeight = smr.GetBlendShapeWeight(0);
+        float newWeight = 0;
+
+        if(goal > currWeight && currWeight <= 101)
+        {
+            Debug.Log("Beginning blend loop!");
+
+            while (currWeight < goal)
+            {
+                newWeight = currWeight + blendSpeed;
+                smr.SetBlendShapeWeight(0, newWeight);
+
+                currWeight = newWeight;
+
+                yield return new WaitForSeconds(0.005f);
+            }
+
+        }
+        else if(goal < currWeight && currWeight >= 1){
+
+
+            Debug.Log("Beginning blend loop!");
+
+            while (currWeight > goal)
+            {
+                newWeight = currWeight - blendSpeed;
+                smr.SetBlendShapeWeight(0, newWeight);
+
+                currWeight = newWeight;
+
+                yield return new WaitForSeconds(0.005f);
+            }
+
+        }
+
+        
+    }
+}
---
+++ Assets/triggerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerScript : MonoBehaviour {
+
+    public GameObject blendObject;
+
+    float teethGoal = 0;
+    float faceGoal = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("openTeeth"))
+        {
+            Debug.Log("opening teeth");
+            teethGoal++;
+            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("teethSmile"), teethGoal);
+        }
+        if (Input.GetButton("closeTeeth")){
+            Debug.Log("close teeth");
+            teethGoal--;
+            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("teethSmile"), teethGoal);
+        }
+        if (Input.GetButton("happyFace")){
+            Debug.Log("happy face");
+            faceGoal--;
+            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("Group3"), faceGoal);
+        }
+        if (Input.GetButton("sadFace")){
+            Debug.Log("sad face");
+            faceGoal++;
+            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("Group3"), faceGoal);
+        }
+	
+	}
+}
