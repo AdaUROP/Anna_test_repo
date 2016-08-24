3e726fa8a90a2f8ed6f918cb6ad6703811318255
Anna Sikkink <asikkink@hawaii.edu>
2016-08-24 12:08:19 -1000

    Commented code and made sure everything works!

--- Assets/changeBlendshape.cs
+++ Assets/changeBlendshape.cs
@@ -1,69 +1,68 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class changeExpression : MonoBehaviour {
+public class changeBlendshape : MonoBehaviour {
 
-    public float blendSpeed = 1f;
+    public float blendSpeed = 1f; //this is the speed of blending, also assignable from unity editor
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
+	//wrapper class to blend shapes, takes the object to blend and the goal weight to blend to
 	public void blendShape(GameObject blend, float goal){
 
+        //in case vwe are given an object that does not blend, try/catch the call to retrieve the skinned mesh renderer on the object
         try {
             SkinnedMeshRenderer smr = blend.GetComponent<SkinnedMeshRenderer>();
-            StartCoroutine(blendToGoal(smr, goal));
+            StartCoroutine(blendToGoal(smr, goal)); //once we have it, start the coroutine to blend
         }
         catch
-        {
+        {   //if there is not a blendshape, print to the log
             Debug.Log("Object does not have a blendshape SkinnedMeshRenderer Component.");
         }
 		
 	}
 	
-	//write a basic function to call the coroutines given an object to blend
-	
-	//coroutine should check goal against max and min, and see if the change is pos or neg (curr vs goal)
-	//then update the blend weight by the speed in a while loop until we get to the goal
+
+    //coroutine to blend the shape to the weight goal
     IEnumerator blendToGoal(SkinnedMeshRenderer smr, float goal)
     {
+        //retrieve the current weight and initialize the newWeight variable
         float currWeight = smr.GetBlendShapeWeight(0);
         float newWeight = 0;
 
+        //check if this is a positive change in weight
         if(goal > currWeight && currWeight <= 101)
         {
             Debug.Log("Beginning blend loop!");
 
+            //adjust the weight by the blendspeed until we hit the goal weight
             while (currWeight < goal)
             {
                 newWeight = currWeight + blendSpeed;
                 smr.SetBlendShapeWeight(0, newWeight);
 
+                //update the current weight
                 currWeight = newWeight;
 
+                //pause for a small amount of time so the change is gradual
                 yield return new WaitForSeconds(0.005f);
             }
 
         }
+        //else this is a negative change in weight
         else if(goal < currWeight && currWeight >= 1){
 
 
             Debug.Log("Beginning blend loop!");
 
+            //adjust the weight by the blendspeed until we hit the goal weight
             while (currWeight > goal)
             {
                 newWeight = currWeight - blendSpeed;
                 smr.SetBlendShapeWeight(0, newWeight);
 
+                //update the current weight
                 currWeight = newWeight;
 
+                //pause for a small amount of time so the change is gradual
                 yield return new WaitForSeconds(0.005f);
             }
 
--- Assets/triggerScript.cs
+++ Assets/triggerScript.cs
@@ -3,39 +3,42 @@ using System.Collections;
 
 public class triggerScript : MonoBehaviour {
 
-    public GameObject blendObject;
+    //this trigger script just adjusts the expression when you hold down keyboard keys. 
+    //We will be writing other trigger scripts for making blendshape changes later based on game state or in game triggers fired
 
+    public GameObject blendObject; //this will be the the object with the changeBlendshape script attached
+
+    //individual blendshape goals
     float teethGoal = 0;
     float faceGoal = 0;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetButton("openTeeth"))
+        if (Input.GetButton("openTeeth")) //input set to when "A" key is held down
         {
             Debug.Log("opening teeth");
-            teethGoal++;
-            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("teethSmile"), teethGoal);
+            teethGoal++; //increase the goal by 1
+            //call the blendshape function on the blendObject to the shape we want to change to the new goal amount
+            blendObject.GetComponent<changeBlendshape>().blendShape(GameObject.Find("teethSmile"), teethGoal); 
         }
-        if (Input.GetButton("closeTeeth")){
+        if (Input.GetButton("closeTeeth")){ //input set to when "D" key is held down
             Debug.Log("close teeth");
-            teethGoal--;
-            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("teethSmile"), teethGoal);
+            teethGoal--; //decrease the goal by 1
+            //call the blendshape function on the blendObject to the shape we want to change to the new goal amount
+            blendObject.GetComponent<changeBlendshape>().blendShape(GameObject.Find("teethSmile"), teethGoal);
         }
-        if (Input.GetButton("happyFace")){
+        if (Input.GetButton("happyFace")){ //input set to when "W" key is held down
             Debug.Log("happy face");
-            faceGoal--;
-            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("Group3"), faceGoal);
+            faceGoal--; //decrease the goal by 1
+            //call the blendshape function on the blendObject to the shape we want to change to the new goal amount
+            blendObject.GetComponent<changeBlendshape>().blendShape(GameObject.Find("Group3"), faceGoal);
         }
-        if (Input.GetButton("sadFace")){
+        if (Input.GetButton("sadFace")){ //input set to when "S" key is held down
             Debug.Log("sad face");
-            faceGoal++;
-            blendObject.GetComponent<changeExpression>().blendShape(GameObject.Find("Group3"), faceGoal);
+            faceGoal++; //increase the goal
+            //call the blendshape function on the blendObject to the shape we want to change to the new goal amount
+            blendObject.GetComponent<changeBlendshape>().blendShape(GameObject.Find("Group3"), faceGoal);
         }
 	
 	}
