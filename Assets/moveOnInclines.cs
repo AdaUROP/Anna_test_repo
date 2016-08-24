dc52c86e3d1633a6f3797500fe35f7b5ebec7908
Anna Sikkink <asikkink@hawaii.edu>
2016-08-23 19:25:34 -1000

    Made a few changes to the script, should work now

--- Assets/moveOnInclines.cs
+++ Assets/moveOnInclines.cs
@@ -5,9 +5,11 @@ public class moveOnInclines : MonoBehaviour {
 
     float initDistanceFromGround;
     RaycastHit hit;
+    Camera viveCam; // camera should be assigned from Unity editor
 
     // Use this for initialization
     void Start () {
+
         //find the difference in distance from the camera to the ground
         if (Physics.Raycast(transform.position, -Vector3.up, out hit))
         {
@@ -21,18 +23,22 @@ public class moveOnInclines : MonoBehaviour {
 
     void Update()
     {
-        
 
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        //check if camera moved (get camera y pos)
+        Vector3 camPos = viveCam.gameObject.transform.position;
+
+        //raycast from camera and see what we collide with
+
+        if (Physics.Raycast(camPos, -Vector3.up, out hit))
         {
             print("Found object " + hit.collider.gameObject.name +" - distance: " + hit.distance);
             Debug.DrawLine(transform.position, hit.point);
 
             //set the distance of the camera from the ground to match our initial value
             if (hit.collider.gameObject.tag == "ground")
-            {
+            { //set the room y location to match the position where the ray collided
                 print("hit pt y: " + hit.point.y + " intD: "+initDistanceFromGround+" new y: " + (hit.point.y + initDistanceFromGround));
-                gameObject.transform.position = new Vector3(gameObject.transform.position.x, hit.point.y + initDistanceFromGround, gameObject.transform.position.z);
+                gameObject.transform.position = new Vector3(camPos.x, hit.point.y + initDistanceFromGround, camPos.z);
             }
         }
     }
